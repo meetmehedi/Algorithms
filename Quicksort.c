74109c31d69c72b28d46040233d6520a93656d7a
MD. Mehedi Hasan <167318121+meetmehedi@users.noreply.github.com>
2025-08-23 19:08:53 +0600

    Add files via upload

---
+++ Quicksort.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
+/*
+Sample Test Case 1:
+Input:
+Enter number of elements: 6
+Enter 6 integers:
+34 7 23 32 5 62
+
+Output:
+Original array: 34 7 23 32 5 62
+Sorted array: 5 7 23 32 34 62
+
+Sample Test Case 2:
+Input:
+Enter number of elements: 5
+Enter 5 integers:
+9 1 8 2 7
+
+Output:
+Original array: 9 1 8 2 7
+Sorted array: 1 2 7 8 9
+*/
